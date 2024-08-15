80d8091fc7815949a0f692d7df2f8867ade6fa35
shenmuxin <shenjiahao0610@163.com>
2024-08-15 11:05:10 +0800

    finish sys_trace

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,7 @@ found:
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
 
+  p->syscall_trace = 0; // 为 syscall_trace 设置一个 0 的默认值
   return p;
 }
 
@@ -291,6 +292,8 @@ fork(void)
   np->cwd = idup(p->cwd);
 
   safestrcpy(np->name, p->name, sizeof(p->name));
+
+  np->syscall_trace = p->syscall_trace; // 子进程继承父进程的 syscall_trace
 
   pid = np->pid;
 
--- kernel/syscall.h
+++ kernel/syscall.h
@@ -20,3 +20,4 @@
 #define SYS_link   19
 #define SYS_mkdir  20
 #define SYS_close  21
+#define SYS_trace  22   // 新增sys_trace系统调用
